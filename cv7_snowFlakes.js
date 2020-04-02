802442ea895ff9c67ea12191cf11642a2e3056a4
Spongebob971 <43585277+Spongebob971@users.noreply.github.com>
2020-04-02 16:52:27 +0200

    L patterns make it work haha.

--- cv7_snowFlakes.js
+++ cv7_snowFlakes.js
@@ -1,16 +1,32 @@
-//L-system
+let axiom1 = ["F", "+", "F", "+", "F", "+", "F"];
+let axiomRule1 = [
+  "F",
+  "+",
+  "F",
+  "-",
+  "F",
+  "-",
+  "F",
+  "F",
+  "+",
+  "F",
+  "+",
+  "F",
+  "-",
+  "F"
+];
+let axiomAngle1 = 90;
 
-let const_distance = 1;
-let degree = 0; //
-let actualPos = {
-  x: 0,
-  y: 0
-};
+let axiom2 = ["F", "+", "+", "F", "+", "+", "F"];
+let axiomRule2 = ["F", "+", "F", "-", "-", "F", "+", "F"];
+let axiomAngle2 = 60;
 
-let array1 = ["F", "+", "F", "-", "F", "F", "+", "F", "+", "F", "-", "F"];
-let array2 = ["F", "+", "F", "-", "-", "F", "+", "F"];
-let array3 = ["F", "[", "+", "F", "]", "F", "[", "-", "F", "]", "F"];
-let array4 = [
+let axiom3 = ["F"];
+let axiomRule3 = ["F", "[", "+", "F", "]", "F", "[", "-", "F", "]", "F"];
+let axiomAngle3 = 180 / 7;
+
+let axiom4 = ["F"];
+let axiomRule4 = [
   "F",
   "F",
   "+",
@@ -32,27 +48,32 @@ let array4 = [
   "F",
   "]"
 ];
+let axiomAngle4 = 180 / 8;
 
-var pathArray = new Array(4);
-
-for (var i = 0; i < pathArray.length; i++) {
-  pathArray[i] = new Array(100);
-}
+var pathArray = [];
 
 export function cv7doTheWork() {
-  //1. F+F-F-FF+F+F-F
-  //2. F+F--F+F
-  //3. F[+F]F[-F]F
-  //4. FF+[+F-F-F]-[-F+F+F]
+  // switch axiom rule and angle to obtain different pattern.
+  let axiom = axiom1;
+  let rule = axiomRule1;
+  let axiomAngle = axiomAngle1;
 
-  computePath(array1, pathArray[0]);
+  let arrayWithReplacedFs = axiom;
+  let result = [];
 
-  let secondDetail = replaceFs(array1, array2);
-  //let thirdDetail = replaceFs(secondDetail, array3);
+  let iterations = 3;
+  // change value of iterations so result is much more detailed.
+  for (let i = 0; i < iterations; i++) {
+    arrayWithReplacedFs = replaceFs(arrayWithReplacedFs, rule);
 
-  computePath(secondDetail, pathArray[1]);
-  //computePath(thirdDetail, pathArray[2]);
-  draw(3);
+    result = computePath(arrayWithReplacedFs, axiomAngle);
+  }
+
+  result.forEach(element => {
+    pathArray.push(element);
+  });
+
+  draw();
 }
 
 function replaceFs(arrayToReplace, replacingArray) {
@@ -73,8 +94,6 @@ function replaceFs(arrayToReplace, replacingArray) {
   for (let k = 0; k < indexes.length; k++) {
     result.splice(indexes[k], 1);
 
-    //debugger;
-
     result.splice.apply(result, [indexes[k], 0].concat(replacingArray));
 
     //update indexes
@@ -85,67 +104,107 @@ function replaceFs(arrayToReplace, replacingArray) {
   return result;
 }
 
-function computePath(array, pathArray) {
-  degree = 0;
+function computePath(array, angle) {
+  let degree = 90;
+  let angleToRemember = [];
+  let temporaryPath2 = []; // when "[" occurs, push to result Array points from returnPath that contains same points after "[" but backwards. All that action so graph won't be damaged.
+  let result = [];
 
-  let startingPoint = {
-    x: 0,
-    y: 0
+  let newPoint = {
+    x: 10,
+    y: 10
   };
 
-  pathArray.push(startingPoint);
-
-  let i = 0;
+  result.push(newPoint);
 
   array.forEach(element => {
     if (element == "F") {
-      let newPoint = {
-        x: actualPos.x + 1 * Math.cos(degree),
-        y: actualPos.y + 1 * Math.sin(degree)
+      newPoint = {
+        x: newPoint.x + 1 * Math.cos(degree * (Math.PI / 180)),
+        y: newPoint.y + 1 * Math.sin(degree * (Math.PI / 180))
       };
 
-      actualPos.x = newPoint.x;
-      actualPos.y = newPoint.y;
+      result.push(newPoint);
 
-      pathArray.push(newPoint);
+      if (temporaryPath2.length != 0) {
+        temporaryPath2[temporaryPath2.length - 1].push(newPoint);
+      }
     } else if (element == "+") {
-      degree += 45;
+      degree += angle;
       degree = degree > 360 ? degree - 360 : degree;
     } else if (element == "-") {
-      let newDegree = degree - 45;
+      let newDegree = degree - angle;
       degree = newDegree < 0 ? 360 + newDegree : newDegree;
+    } else if (element == "[") {
+      let tmp = result[result.length - 1];
+
+      angleToRemember.push(
+        parseFloat((Math.round(degree * 10) / 10).toFixed(0))
+      );
+
+      temporaryPath2.push([]);
+
+      temporaryPath2[temporaryPath2.length - 1].push(tmp);
+
+      console.log(temporaryPath2);
+    } else if (element == "]") {
+      degree = angleToRemember[angleToRemember.length - 1];
+
+      angleToRemember.pop();
+
+      temporaryPath2[temporaryPath2.length - 1]
+        .slice()
+        .reverse()
+        .forEach(elementReversePath => {
+          result.push(elementReversePath);
+        });
+
+      newPoint = result[result.length - 1]; // newPoint je actual point od ktere se pak pokracuje, proto musi byt posledni bod z result newPoint
+
+      temporaryPath2.pop();
     }
   });
+
+  return result;
 }
 
-function draw(amount) {
+function draw() {
   let traces = [];
 
   let data = [];
 
-  for (let i = 0; i < amount; i++) {
-    let xArray = [];
-    let yArray = [];
+  let xArray = [];
+  let yArray = [];
 
-    pathArray[i].forEach(element => {
-      xArray.push(element.x);
-      yArray.push(element.y);
-    });
+  pathArray.forEach(element => {
+    xArray.push(element.x);
+    yArray.push(element.y);
+  });
 
-    let trace = {
-      x: xArray,
-      y: yArray,
-      type: "scatter"
-    };
+  let trace = {
+    x: xArray,
+    y: yArray,
+    type: "scatter"
+  };
 
-    traces.push(trace);
-  }
+  traces.push(trace);
 
   traces.forEach(trace => {
     data.push(trace);
   });
 
-  //var data = [trace1, trace2];
+  var layout = {
+    autosize: false,
+    width: 800,
+    height: 800,
+    margin: {
+      l: 50,
+      r: 50,
+      b: 100,
+      t: 100,
+      pad: 4
+    }
+  };
 
-  Plotly.newPlot("myDiv", data);
+  Plotly.newPlot("myDiv", data, layout);
 }
